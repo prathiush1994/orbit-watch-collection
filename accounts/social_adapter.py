b99a78f34045e06f52209fbcba445f0f9948b30d
Prathiush PP <prathiush1994@gmail.com>
2026-06-05 13:03:31 +0530

    Fix Google social login account activation

--- accounts/social_adapter.py
+++ accounts/social_adapter.py
@@ -1,17 +1,25 @@
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from allauth.account.adapter import DefaultAccountAdapter
-from accounts.models import Account
 
 
 class MyAccountAdapter(DefaultAccountAdapter):
+
     def populate_username(self, request, user):
-        pass  # No username field in this project
+        pass
 
     def save_user(self, request, user, form, commit=True):
-        user = super().save_user(request, user, form, commit=False)
-        user.is_active = False  
+        user = super().save_user(
+            request,
+            user,
+            form,
+            commit=False
+        )
+
+        user.is_active = True
+
         if commit:
             user.save()
+
         return user
 
 
