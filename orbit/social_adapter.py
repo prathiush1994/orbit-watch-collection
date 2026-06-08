483a85bf3b7f2be86fd90310a1dde3ff63285367
Prathiush PP <prathiush1994@gmail.com>
2026-06-08 22:17:36 +0530

    Activate and verify users authenticated through Google OAuth

--- orbit/social_adapter.py
+++ orbit/social_adapter.py
@@ -1,20 +1,13 @@
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from accounts.models import Account
-import logging
-
-logger = logging.getLogger(__name__)
 
 
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
 
     def is_auto_signup_allowed(self, request, sociallogin):
-        logger.warning("AUTO SIGNUP ALLOWED")
         return True
 
     def pre_social_login(self, request, sociallogin):
-        logger.warning("PRE SOCIAL LOGIN")
-        logger.warning(f"EMAIL = {sociallogin.user.email}")
-
         try:
             user = Account.objects.get(email=sociallogin.user.email)
 
@@ -27,16 +20,10 @@ class MySocialAccountAdapter(DefaultSocialAccountAdapter):
             pass
 
     def save_user(self, request, sociallogin, form=None):
-        logger.warning("SOCIAL SAVE USER START")
-
         user = super().save_user(request, sociallogin, form)
-
-        logger.warning(f"USER CREATED: {user.email}")
 
         user.is_active = True
         user.email_verified = True
         user.save()
-
-        logger.warning("SOCIAL SAVE USER END")
 
         return user
