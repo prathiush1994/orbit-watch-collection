3164563abe19c882b19efc3adec7c4641d9a6c57
Prathiush PP <prathiush1994@gmail.com>
2026-06-08 21:18:24 +0530

    Add Google OAuth authentication error logging

--- orbit/social_adapter.py
+++ orbit/social_adapter.py
@@ -8,17 +8,22 @@ logger.warning("SOCIAL ADAPTER FILE LOADED")
 
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
 
-    def __init__(self, *args, **kwargs):
-        logger.warning("SOCIAL ADAPTER INSTANCE CREATED")
-        super().__init__(*args, **kwargs)
+    def on_authentication_error(
+        self,
+        request,
+        provider,
+        error=None,
+        exception=None,
+        extra_context=None,
+    ):
+        logger.error(f"PROVIDER = {provider}")
+        logger.error(f"ERROR = {error}")
+        logger.error(f"EXCEPTION = {exception}")
 
-    def is_auto_signup_allowed(self, request, sociallogin):
-        logger.warning("AUTO SIGNUP ALLOWED")
-        return True
-
-    def pre_social_login(self, request, sociallogin):
-        logger.warning("PRE SOCIAL LOGIN")
-
-    def save_user(self, request, sociallogin, form=None):
-        logger.warning("SOCIAL SAVE USER")
-        return super().save_user(request, sociallogin, form)
+        return super().on_authentication_error(
+            request,
+            provider,
+            error,
+            exception,
+            extra_context,
+        )
