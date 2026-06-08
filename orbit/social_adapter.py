9fd25607d226683177969b3af5f4658b33dfd642
Prathiush PP <prathiush1994@gmail.com>
2026-06-08 21:12:59 +0530

    Remove temporary Google OAuth adapter debugging logs

--- orbit/social_adapter.py
+++ orbit/social_adapter.py
@@ -1,5 +1,4 @@
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-from allauth.account.adapter import DefaultAccountAdapter
 import logging
 
 logger = logging.getLogger(__name__)
@@ -9,17 +8,17 @@ logger.warning("SOCIAL ADAPTER FILE LOADED")
 
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
 
+    def __init__(self, *args, **kwargs):
+        logger.warning("SOCIAL ADAPTER INSTANCE CREATED")
+        super().__init__(*args, **kwargs)
+
     def is_auto_signup_allowed(self, request, sociallogin):
         logger.warning("AUTO SIGNUP ALLOWED")
-        print("logger.warning(AUTO SIGNUP ALLOWED)")
         return True
 
     def pre_social_login(self, request, sociallogin):
         logger.warning("PRE SOCIAL LOGIN")
-        logger.warning(f"EMAIL = {sociallogin.user.email}")
-        print("logger.warning(PRE SOCIAL LOGIN) /n logger.warning()",f"EMAIL = {sociallogin.user.email}")
 
     def save_user(self, request, sociallogin, form=None):
         logger.warning("SOCIAL SAVE USER")
-        print("logger.warning(SOCIAL SAVE USER)")
         return super().save_user(request, sociallogin, form)
