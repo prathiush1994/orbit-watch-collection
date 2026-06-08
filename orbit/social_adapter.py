141ec5d2fe506f0c0c83c31b414fe699500a3ac2
Prathiush PP <prathiush1994@gmail.com>
2026-06-08 21:59:10 +0530

    Activate and verify Google OAuth users automatically

--- orbit/social_adapter.py
+++ orbit/social_adapter.py
@@ -1,34 +1,35 @@
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from accounts.models import Account
 import logging
 
 logger = logging.getLogger(__name__)
 
-logger.warning("SOCIAL ADAPTER FILE LOADED")
-
 
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
 
-    def on_authentication_error(
-        self,
-        request,
-        provider,
-        error=None,
-        exception=None,
-        extra_context=None,
-    ):
-        logger.error(f"PROVIDER = {provider}")
-        logger.error(f"ERROR = {error}")
-        logger.error(f"EXCEPTION = {exception}")
-        logger.error(f"EXTRA_CONTEXT = {extra_context}")
+    def is_auto_signup_allowed(self, request, sociallogin):
+        logger.warning("AUTO SIGNUP ALLOWED")
+        return True
 
-        return super().on_authentication_error(
-            request,
-            provider,
-            error,
-            exception,
-            extra_context,
-        )
-    
     def pre_social_login(self, request, sociallogin):
-    logger.error("PRE SOCIAL LOGIN REACHED")
-    logger.error(f"EMAIL={sociallogin.user.email}")
+        logger.warning("PRE SOCIAL LOGIN")
+        logger.warning(f"EMAIL = {sociallogin.user.email}")
+
+        try:
+            user = Account.objects.get(email=sociallogin.user.email)
+
+            if not user.is_active:
+                user.is_active = True
+                user.email_verified = True
+                user.save()
+
+        except Account.DoesNotExist:
+            pass
+
+    def save_user(self, request, sociallogin, form=None):
+        logger.warning("SOCIAL SAVE USER")
+        user = super().save_user(request, sociallogin, form)
+        user.is_active = True
+        user.email_verified = True
+        user.save()
+        return user
